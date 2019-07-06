69525d11a5a8afe0447efb62b1722e6257d5c04f
Matongo2010 <justineadriano638@gmail.com>
2019-07-06 22:22:36 +0300

    define all the input in the form

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  quotes: any;
+  names: any;
+
+  addNewQuote(quote) {
+    let nameLength = this.names.length;
+    let quoteLength = this.quotes.length;
+    quote.newDate = new Date(quote.newDate)
+    this.quotes.push(quote)
+
+  }
 
 
   constructor() { }
