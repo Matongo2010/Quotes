3c131ca4a020a3b668874c003873f24076ea116c
Matongo2010 <justineadriano638@gmail.com>
2019-07-08 04:51:34 +0300

    Change author to description

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -2,7 +2,7 @@ export class Quotes {
     showDescription: boolean;
     constructor(public name:string,
         public quote:string, 
-        public author:string, 
+        public description:string, 
         public date:Date)
         {
         this.showDescription = false;
