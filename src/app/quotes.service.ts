94866aa4fb2f49ecf094eb41614bb690ebbb4fa9
Matongo2010 <justineadriano638@gmail.com>
2019-07-10 12:00:25 +0300

    imported class Quotes in the new file and exported the arrays

---
+++ src/app/quotes.service.ts
@@ -0,0 +1,8 @@
+import { Quotes } from './quotes';
+
+
+export var quotes:Quotes[]=[
+    new Quotes(1, 'If you have the right vision, and you take the right steps, then success is inevitable', 'Bob Collymore', new Date(2019,5,6)),
+  new Quotes(2, 'None but ourselves can free our minds','Bob Marley',new Date(2019,3,6)),
+  new Quotes(3, 'One good thing about music, when it hits you, you feel no pain', 'Bob Marley',new Date(2019,7,2)),
+]
